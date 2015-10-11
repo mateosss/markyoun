a4f7e73deb3d080ef09b1d28a6ddf66f71b7115e
mateosss <mateodemayo@gmail.com>
2015-10-11 20:31:31 -0300

    Mejorado instalador para linux

---
+++ instalador.sh
@@ -0,0 +1,46 @@
+echo "\n\n\n\n#-----Comenzando instalación de MarkYoun-----#\n\n\n\n"
+
+echo "\n\n\n\n#-----Eliminando versiones anteriores de markyoun...-----#\n\n\n\n"
+sudo rm -rf /usr/share/MarkYoun/
+sudo rm /usr/applications/MarkYoun.desktop
+sudo rm /bin/markyoun
+
+echo "\n\n\n\n#-----Descargando componentes necesarios para la instalación-----#"
+echo "unzip, python, python-qt4, pandoc, python-pip, librerías python markdown y pypandoc\n\n\n\n"
+sudo apt-get install python -y
+sudo apt-get install python-qt4 -y
+sudo apt-get install pandoc -y
+sudo apt-get install python-pip -y
+sudo pip install markdown
+sudo pip install pypandoc
+
+echo "\n\n\n\n#-----Descargando MarkYoun desde dropbox...-----#\n\n\n\n"
+wget https://www.dropbox.com/s/344jmx41hqulpdl/MarkYoun.zip
+echo "\n\n\n\nDescomprimiendo...\n\n\n\n"
+sudo mkdir /usr/
+sudo mkdir /usr/share/
+sudo unzip -o MarkYoun.zip -d /usr/share/MarkYoun/
+
+echo "\n\n\n\n#-----¿Desea descargar texlive y texlive-base?...-----#"
+echo "Puede elegir no hacerlo, pero no podrá convertir sus archivos al formato pdf\n\n\n\n"
+sudo apt-get install texlive texlive-base
+
+echo "\n\n\n\n#-----Configurando compatibilidad con el sistema-----#"
+cd /usr/share/MarkYoun/
+chmod +x MarkYoun
+sudo cp /usr/share/MarkYoun/MarkYoun /bin/markyoun
+sudo cp /usr/share/MarkYoun/MarkYoun.desktop /usr/share/applications/MarkYoun.desktop
+
+#echo "\n\n\n\nUbicandolo en el escritorio, si no tiene el ejecutable estará en /home/USTED/MarkYoun/\n\n\n\n"
+#cp $HOME/MarkYoun/MarkYoun $HOME/Escritorio/MarkYoun
+#cp $HOME/MarkYoun/MarkYoun $HOME/Desktop/MarkYoun
+#cp $HOME/MarkYoun/MarkYoun $HOME/MarkYoun
+
+echo "\n\n\n\nEjecutando MarkYoun por primera vez...\n\n\n\n"
+markyoun
+
+echo "\n\n\n\nEliminando restos del instalador de MarkYoun de su equipo\n\n\n\n"
+cd $HOME/
+rm -f $HOME/MarkYoun.zip
+rm -f $HOME/Downloads/distribute_setup.py
+rm -f $HOME/instalador.sh
---
+++ sprint.md
@@ -0,0 +1,25 @@
+descompirmir todo el programa en /usr/share/markyoun
+
+modificar el sh para que calce con la nueva ubicacion
+
+mover el sh a /bin/markyoun, para crear variable de entorno
+
+crear entrada .desktop en /usr/share/applications/MarkYoun.desktop:
+[Desktop Entry]
+Name=MarkYoun
+Exec=markyoun
+Icon=/home/$LOGNAME/MarkYoun/Nuevos Iconos/Creando Iconos/svg/icono.png
+Type=Application
+Categories=GTK;GNOME;Utility;
+
+que el zip este en github
+
+fijarse como hacer para que el icono funcione
+
+y fijarse como hacer para que abra archivos .md directamente
+
+desactivar que transforme automaticamente a html
+
+reorganizar carpetas, borrar archivos inutiles
+
+rearmar anotaciones, entender bien version actual
